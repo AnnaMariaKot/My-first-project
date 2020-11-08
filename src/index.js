7df28f03d3cf29e608a61aa1718d9e4b9930bd9c
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2020-11-08 15:20:50 +0100

    Working on index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function displayTemperature(response) {
+    console.log(response.data.main.temp)
+}
 
+
+let apiKey = "34f1c513b53f57aa71f2dd7697c02049"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Gent&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature)
