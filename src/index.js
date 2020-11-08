8b62a155770e2f6ad13f2480acc997556e62fc3f
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2020-11-08 17:36:15 +0100

    added an image

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ function displayTemperature(response) {
 
 
 let apiKey = "34f1c513b53f57aa71f2dd7697c02049"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Gent&appid=${apiKey}&units=metric`
+let city = "Lokeren"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
     axios.get(apiUrl).then(displayTemperature);
