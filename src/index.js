2a26fdfd57a8fe0a4cb25279fe0ea311062b5e16
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2020-11-08 15:41:08 +0100

    Added more js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 function displayTemperature(response) {
-    console.log(response.data.main.temp)
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature")
+    let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description")
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name
+    descriptionElement.innerHTML = response.data.weather[0].description;
 }
+   
+
 
 
 let apiKey = "34f1c513b53f57aa71f2dd7697c02049"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Gent&appid=${apiKey}&units=metric`
 
-axios.get(apiUrl).then(displayTemperature)
+    axios.get(apiUrl).then(displayTemperature);
